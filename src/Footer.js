efdd19c090f1cc7df7345edb12c8b2b67be4c4d6
rimjhimart <rimjhim.gupta.ak@gmail.com>
2025-01-18 23:54:09 +0530

    first

---
+++ src/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div id="footer">
+      <div id="shape1"></div>
+      <div id="shape2"></div>
+      <div id="footerupper">
+        <div>
+          <h2>Aim</h2>
+          <h2>Analysis</h2>
+          <h2>Ask</h2>
+        </div>
+        <div id="email">
+          <p>Unlock your potential with our expert guidance. Explore, plan, and achieve your academic dreams with personalized counseling tailored just for you.</p>
+          <h6>Email Address</h6>
+        </div>
+      </div>
+      <div id="footerlower">
+        <h1>three-tap</h1>
+        <div>
+          <h5>Copyright © Three-Tap</h5>
+          <h5>X</h5>
+          <h5>Instagram</h5>
+          <h5>LinkedIn</h5>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
