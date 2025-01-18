e3125761d51dd284b34a3f97e6d214e270badb5e
rimjhimart <rimjhim.gupta.ak@gmail.com>
2025-01-18 23:53:52 +0530

    first

---
+++ src/FixedImage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './FixedImage.module.css';
+
+function FixedImage() {
+  return (
+    <div id={styles['fixed-image']}></div>
+  );
+}
+
+export default FixedImage;
